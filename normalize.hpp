491a3505eb2c478337e21deec983619218994c5c
jay.wu <jay.wu@enflame-tech.com>
2024-10-22 16:35:53 +0800

    discard useless anonymous prefix: __choreo_ca.

--- normalize.hpp
+++ normalize.hpp
@@ -314,7 +314,7 @@ struct Normalizer : public Visitor {
 
         // else, hoist the arith out
         int index = cur_dma_index + mnodes_insertions[multi_nodes.top()].size();
-        auto nname = "__choreo_ca_" + SymbolTable::GetAnonName();
+        auto nname = SymbolTable::GetAnonName();
         mnodes_insertions[multi_nodes.top()].emplace_back(
             std::make_tuple(index, v, nname));
         repls.emplace_back(i, AST::Make<AST::Identifier>(v->LOC(), nname));
