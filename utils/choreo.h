851caf3e42b02806e26bf2be8f9d95df9d39df91
Garfee Guan <xiaofeng.guan@enflame-tech.com>
2024-11-15 13:46:30 +0800

    Rename files for consistency.

--- utils/choreo.h
+++ utils/choreo.h
@@ -5,12 +5,12 @@
 // #error "Choreo requires C++17 or later"
 #endif
 
+#include <algorithm>
 #include <cstdint>          // For fixed-width integer types
 #include <initializer_list> // for std::initializer_list
 #include <iostream>         // report error
 #include <memory>
 #include <random>
-#include <algorithm>
 
 namespace choreo {
 
@@ -68,9 +68,7 @@ public:
   T* end() { return data + N; }
   const T* end() const { return data + N; }
 
-  void fill_random() {
-    fill_random(data, std::is_floating_point<T>());
-  }
+  void fill_random() { fill_random(data, std::is_floating_point<T>()); }
 
 private:
   T data[N];
@@ -78,24 +76,25 @@ private:
   template <typename U>
   typename std::enable_if<std::is_floating_point<U>::value>::type
   fill_random(U (&array)[N], std::true_type) {
-      std::random_device rd;
-      std::mt19937 gen(rd());
-      std::uniform_real_distribution<U> rand_func(-1.0, 1.0); // 浮点数范围 [-1.0, 1.0)
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<U> rand_func(-1.0,
+                                                1.0); // 浮点数范围 [-1.0, 1.0)
 
-      std::generate_n(&array[0], N, [&]() { return rand_func(gen); });
+    std::generate_n(&array[0], N, [&]() { return rand_func(gen); });
   }
 
   // 如果 T 是整数类型，使用 std::uniform_int_distribution
   template <typename U>
   typename std::enable_if<std::is_integral<U>::value>::type
   fill_random(U (&array)[N], std::false_type) {
-      std::random_device rd;
-      std::mt19937 gen(rd());
-      std::uniform_int_distribution<U> rand_func(-100, 100); // 整数范围 [-100, 100]
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<U> rand_func(-100,
+                                               100); // 整数范围 [-100, 100]
 
-      std::generate_n(&array[0], N, [&]() { return rand_func(gen); });
+    std::generate_n(&array[0], N, [&]() { return rand_func(gen); });
   }
-
 };
 
 template <typename T, size_t N, size_t M>
@@ -221,29 +220,31 @@ public:
   }
 
   void fill_random(T lb, T ub) {
-    fill_random(this->data(), this->size(), std::is_floating_point<T>(), lb, ub);
+    fill_random(this->data(), this->size(), std::is_floating_point<T>(), lb,
+                ub);
   }
 
 private:
   template <typename U>
   typename std::enable_if<std::is_floating_point<U>::value>::type
   fill_random(U* array, size_t N, std::true_type, U lb, U ub) {
-      std::random_device rd;
-      std::mt19937 gen(rd());
-      std::uniform_real_distribution<U> rand_func(lb, ub); // 浮点数范围 [-1.0, 1.0)
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<U> rand_func(lb,
+                                                ub); // 浮点数范围 [-1.0, 1.0)
 
-      std::generate_n(&array[0], N, [&]() { return rand_func(gen); });
+    std::generate_n(&array[0], N, [&]() { return rand_func(gen); });
   }
 
   // 如果 T 是整数类型，使用 std::uniform_int_distribution
   template <typename U>
   typename std::enable_if<std::is_integral<U>::value>::type
   fill_random(U* array, size_t N, std::false_type, U lb, U ub) {
-      std::random_device rd;
-      std::mt19937 gen(rd());
-      std::uniform_int_distribution<U> rand_func(lb, ub); // 整数范围 [-100, 100]
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<U> rand_func(lb, ub); // 整数范围 [-100, 100]
 
-      std::generate_n(&array[0], N, [&]() { return rand_func(gen); });
+    std::generate_n(&array[0], N, [&]() { return rand_func(gen); });
   }
 };
 
--- utils/choreo_cuda.h
+++ utils/choreo_cuda.h
@@ -345,22 +345,23 @@ private:
   template <typename U>
   typename std::enable_if<std::is_floating_point<U>::value>::type
   fill_random(U* array, size_t N, std::true_type, U lb, U ub) {
-      std::random_device rd;
-      std::mt19937 gen(rd());
-      std::uniform_real_distribution<U> rand_func(lb, ub); // 浮点数范围 [-1.0, 1.0)
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<U> rand_func(lb,
+                                                ub); // 浮点数范围 [-1.0, 1.0)
 
-      std::generate_n(&array[0], N, [&]() { return rand_func(gen); });
+    std::generate_n(&array[0], N, [&]() { return rand_func(gen); });
   }
 
   // 如果 T 是整数类型，使用 std::uniform_int_distribution
   template <typename U>
   typename std::enable_if<std::is_integral<U>::value>::type
   fill_random(U* array, size_t N, std::false_type, U lb, U ub) {
-      std::random_device rd;
-      std::mt19937 gen(rd());
-      std::uniform_int_distribution<U> rand_func(lb, ub); // 整数范围 [-100, 100]
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<U> rand_func(lb, ub); // 整数范围 [-100, 100]
 
-      std::generate_n(&array[0], N, [&]() { return rand_func(gen); });
+    std::generate_n(&array[0], N, [&]() { return rand_func(gen); });
   }
 };
 
