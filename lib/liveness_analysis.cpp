e1866de43e146448ba9a1ef3de77f8063bdd49bd
jay.wu <jay.wu@enflame-tech.com>
2025-05-14 14:35:23 +0800

    [test] robuster execute engine.
    - fix a bug in liveness analysis.
    - update the testcase to support different input.

--- lib/liveness_analysis.cpp
+++ lib/liveness_analysis.cpp
@@ -1071,8 +1071,9 @@ bool LivenessAnalyzer::Visit(AST::Wait& n) {
       auto expr = dyn_cast<AST::Expr>(item);
       assert(IsSymbolOrArrayRef(*item) &&
              "expect either symbol or array reference.");
+      bool is_array_ref = (expr->op == "elemof");
       std::string name;
-      if (expr)
+      if (is_array_ref)
         name = AST::GetArrayBaseSymbol(*expr)->name;
       else
         name = AST::GetIdentifier(*item)->name;
