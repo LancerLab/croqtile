8158cad575c34f37b8dac979b7d6077e42fa30d0
Garfee Guan <garfee.guan@enflame-tech.com>
2026-04-16 12:25:55 +0800

    fix: preprocessor closing brace.
    (cherry-pick: 0746e818f)

--- lib/preprocess.cpp
+++ lib/preprocess.cpp
@@ -1069,6 +1069,8 @@ bool Preprocess::Process(std::istream& input) {
       line_num++;
     } else if (code_partition == CP_CHOREO) {
       HandleOneChoreoLine(line_to_handle);
+      if (code_partition != CP_KERNEL && !cok_codes.empty())
+        cok_codes.pop_back();
       line_num++;
     } else if (code_partition == CP_KERNEL) {
       HandleOneKernelLine(line_to_handle);
